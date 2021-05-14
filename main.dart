274a12ecd6167160d09ed13bbbb11189f3654571
kainatali123 <84183144+kainatali123@users.noreply.github.com>
2021-05-14 16:26:03 +0500

    Done

---
+++ main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Kainat());
+}
+
+class Kainat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: (Scaffold(
+        body: Text("I am Kainat"),
+      )),
+    );
+  }
+}
